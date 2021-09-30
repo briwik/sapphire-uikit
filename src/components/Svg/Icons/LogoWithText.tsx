fbbaf24d970d0c54826ddfce647e5a72fe7ca649
Literally Collective <bluedragon.cstar@outlook.com>
2021-09-30 03:40:16 -0300

    Feat: publish uikit

---
+++ src/components/Svg/Icons/LogoWithText.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+interface LogoProps extends SvgProps {
+  isDark: boolean;
+}
+
+const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
+  const textColor = isDark ? "#FFFFFF" : "#000000";
+  return (
+    <Svg viewBox="0 0 256 256" {...props}>
+      <image width="256" height="256" href="/images/custom/logo-with-text.png" /> 
+    </Svg>
+  );
+};
+
+export default React.memo(Logo, (prev, next) => prev.isDark === next.isDark);
